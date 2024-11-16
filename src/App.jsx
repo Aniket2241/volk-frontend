d8e4c615dccc17630bc7b8a5d9f2772d2fb7a063
Aniket2241 <sprateek507@gmail.com>
2024-11-16 22:52:17 +0530

    video bug solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import VideoPlayer from './Components/Video'; 
-import Dashboard from './Components/Dashboard'; 
+import React, { useState } from 'react';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   const [isVideoCompleted, setIsVideoCompleted] = useState(false);
 
-  useEffect(() => {
-    
-    const videoTimeout = setTimeout(() => {
-      setIsVideoCompleted(true); 
-    }, 12000); 
-
-    
-    return () => clearTimeout(videoTimeout);
-  }, []);
+  const handleVideoEnd = () => {
+    setIsVideoCompleted(true); // Update state when video ends
+  };
 
   return (
     <div className="mt-[2%]">
       {!isVideoCompleted ? (
-        <VideoPlayer /> 
+        <div>
+          <video
+            src="Volk-into.mp4" 
+            aria-hidden="true"
+            playsInline
+            muted
+            autoPlay
+            onEnded={handleVideoEnd} 
+            style={{ width: '98%', height: '90vh' }} 
+          />
+        </div>
       ) : (
         <Dashboard /> 
       )}
